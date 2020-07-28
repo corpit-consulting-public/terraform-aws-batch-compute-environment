9f622650766a3cd3f0ac9796443e3a29df4ca391
Ethan Knox <ethan.knox@health-union.com>
2020-07-28 15:29:26 -0400

    trying updated variable types

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ resource "aws_batch_compute_environment" "batch_environment_managed" {
   compute_environment_name = "${var.compute_environment_name}"
   compute_resources {
     instance_role      = "${var.instance_role}"
-    instance_type      = ["${var.instance_type}"]
+    instance_type      = var.instance_type
     max_vcpus          = "${var.max_vcpus}"
     min_vcpus          = "${var.min_vcpus}"
-    security_group_ids = ["${var.security_group_ids}"]
-    subnets            = ["${var.subnets}"]
+    security_group_ids = var.security_group_ids
+    subnets            = var.subnets
     type               = "${var.compute_resources_type}"
   }
 
