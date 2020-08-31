2f75d518d163b3cc3c00480aedd0a0a7f3a906b4
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-08-31 16:00:13 -0300

    new module tf12 demo

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
 resource "aws_batch_compute_environment" "batch_environment_managed" {
-  count                 = "${var.compute_resources_type == "SPOT" ? 0 : 1}"
-  compute_environment_name = "${var.compute_environment_name}"
+  count                    = var.compute_resources_type == "SPOT" ? 0 : 1
+  compute_environment_name = var.compute_environment_name
   compute_resources {
-    instance_role      = "${var.instance_role}"
-    instance_type      = ["${var.instance_type}"]
-    max_vcpus          = "${var.max_vcpus}"
-    min_vcpus          = "${var.min_vcpus}"
-    desired_vcpus      = "${var.desired_vcpus}"
-    security_group_ids = ["${var.security_group_ids}"]
-    subnets            = ["${var.subnets}"]
-    type               = "${var.compute_resources_type}"
+    instance_role      = var.instance_role
+    instance_type      = var.instance_type
+    max_vcpus          = var.max_vcpus
+    min_vcpus          = var.min_vcpus
+    desired_vcpus      = var.desired_vcpus
+    security_group_ids = var.security_group_ids
+    subnets            = var.subnets
+    type               = var.compute_resources_type
   }
 
-  service_role         = "${var.service_role}"
-  type                 = "${var.type}"
+  service_role = var.service_role
+  type         = var.type
 }
 
 resource "aws_batch_compute_environment" "batch_environment_spot" {
-  count                    = "${var.compute_resources_type == "SPOT" ? 1 : 0}"
-  compute_environment_name = "${var.compute_environment_name}"
+  count                    = var.compute_resources_type == "SPOT" ? 1 : 0
+  compute_environment_name = var.compute_environment_name
   compute_resources {
-    instance_role      = "${var.instance_role}"
-    instance_type      = ["${var.instance_type}"]
-    max_vcpus          = "${var.max_vcpus}"
-    min_vcpus          = "${var.min_vcpus}"
-    desired_vcpus      = "${var.desired_vcpus}"
-    security_group_ids = ["${var.security_group_ids}"]
-    subnets            = ["${var.subnets}"]
-    type               = "${var.compute_resources_type}"
-    bid_percentage     = "${var.bid_percentage}"
-    spot_iam_fleet_role = "${var.spot_iam_fleet_role}"
-    bid_percentage      = "${var.bid_percentage}"
+    instance_role       = var.instance_role
+    instance_type       = var.instance_type
+    max_vcpus           = var.max_vcpus
+    min_vcpus           = var.min_vcpus
+    desired_vcpus       = var.desired_vcpus
+    security_group_ids  = var.security_group_ids
+    subnets             = var.subnets
+    type                = var.compute_resources_type
+    bid_percentage      = var.bid_percentage
+    spot_iam_fleet_role = var.spot_iam_fleet_role
+    bid_percentage      = var.bid_percentage
   }
 
-  service_role         = "${var.service_role}"
-  type                 = "${var.type}"
+  service_role = var.service_role
+  type         = var.type
 }
 
