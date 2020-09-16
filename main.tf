8011268d6870131cb22460052fb50dceabea4851
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-09-16 15:25:18 -0300

    support batch on terraform 0.12

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_batch_compute_environment" "batch_environment_managed" {
-  count                    = var.compute_resources_type == "SPOT" ? 0 : 1
   compute_environment_name = var.compute_environment_name
   compute_resources {
     instance_role      = var.instance_role
@@ -10,27 +9,8 @@ resource "aws_batch_compute_environment" "batch_environment_managed" {
     security_group_ids = var.security_group_ids
     subnets            = var.subnets
     type               = var.compute_resources_type
-  }
-
-  service_role = var.service_role
-  type         = var.type
-}
-
-resource "aws_batch_compute_environment" "batch_environment_spot" {
-  count                    = var.compute_resources_type == "SPOT" ? 1 : 0
-  compute_environment_name = var.compute_environment_name
-  compute_resources {
-    instance_role       = var.instance_role
-    instance_type       = var.instance_type
-    max_vcpus           = var.max_vcpus
-    min_vcpus           = var.min_vcpus
-    desired_vcpus       = var.desired_vcpus
-    security_group_ids  = var.security_group_ids
-    subnets             = var.subnets
-    type                = var.compute_resources_type
-    bid_percentage      = var.bid_percentage
-    spot_iam_fleet_role = var.spot_iam_fleet_role
-    bid_percentage      = var.bid_percentage
+    bid_percentage      = var.compute_resources_type == "SPOT" ? var.bid_percentage : null
+    spot_iam_fleet_role = var.compute_resources_type == "SPOT" ? spot_iam_fleet_role : null
   }
 
   service_role = var.service_role
